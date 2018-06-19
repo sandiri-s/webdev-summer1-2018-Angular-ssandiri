daa131f3b9b49cd65e7941876edb02356bca4275
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-06-19 15:51:00 -0400

    added css

--- src/app/user-admin/user-admin.component.ts
+++ src/app/user-admin/user-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from "../services/course.service.client";
 import {Course} from "../models/coruse.model.client";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-admin',
@@ -8,10 +9,11 @@ import {Course} from "../models/coruse.model.client";
   styleUrls: ['./user-admin.component.css']
 })
 export class UserAdminComponent implements OnInit {
-  constructor(private service: CourseServiceClient) {
-
+  constructor(private service: CourseServiceClient,  private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.courseId= params['courseId']);
   }
   courses: Course[] = [];
+  courseId="";
   ngOnInit() {
     this.service.findAllCourses()
       .then(courses => this.courses = courses);
