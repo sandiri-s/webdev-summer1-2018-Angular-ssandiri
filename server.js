e215da3f860db3d3af88ffb39bf663ea9bf3a96c
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-06-19 23:07:17 -0400

    added heroku config

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webdev-summer1-angular'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/webdev-summer1-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
